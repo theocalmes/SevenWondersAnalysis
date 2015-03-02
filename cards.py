59f8c31432663383862d40b3a0729b62158fbf8b
Theodore Calmes <theocalmes@gmail.com>
2015-03-01 19:38:42 -0800

    Implemented Scientific structure

--- cards.py
+++ cards.py
@@ -18,7 +18,10 @@ class CardAbstract(object):
 	def can_buy(resources, gold):
 		return contains(self.resource_cost, resources) and self.gold_cost <= gold
 	
-	def is_free_with_cards(cards):
+	def is_free_with_cards(self, cards):
+		for card in cards:
+			if card.name in self.is_free_with:
+				return True
 		return False
 	
 	def reward(self):
@@ -39,18 +42,29 @@ class VictoryPoint(object):
 	def __repr__(self):
 		return "Points: %s" % (self.amount,)
 
-class ResourceCard(CardAbstract):
+class Science(object):
+	def __init__(self, type):
+		self.type = type
+	def __repr__(self):
+		return self.type
+
+class ResourceCardAbstract(CardAbstract):
 	def __init__(self, name, resources, gold_cost):
 		self.name = name
 		self.gold_cost = gold_cost
 		self.resources = resources
-		super(ResourceCard, self).__init__(name)
+		super(ResourceCardAbstract, self).__init__(name)
 		
 	def reward(self):
 		return self.resources
 		
+class RawMaterial(ResourceCardAbstract):
+	pass
+	
+class ManufacturedGood(ResourceCardAbstract):
+	pass
 		
-class BlueCard(CardAbstract):
+class CivilianStructure(CardAbstract):
 	def __init__(self, name, resources_cost, points, is_free_with=None):
 		if is_free_with is None:
 			self.is_free_with = []
@@ -60,24 +74,35 @@ class BlueCard(CardAbstract):
 		self.name = name
 		self.resource_cost = resources_cost
 		self.victory_points = VictoryPoint(points)
-	
-	def is_free_with_cards(self, cards):
-		for card in cards:
-			if card.name in self.is_free_with:
-				return True
-		return False
-
 
 	def reward(self):
 		return self.victory_points
+		
+class ScientificStructure(CardAbstract):
+	def __init__(self, name, resources_cost, science, is_free_with=None):
+		if is_free_with is None:
+			self.is_free_with = []
+		else:
+			self.is_free_with = is_free_with
+					
+		self.name = name
+		self.resource_cost = resources_cost
+		self.science = science
+		
+	def reward(self):
+		return self.science
+		
 
-timber_yard = ResourceCard("Timber Yard", [Resource("wood"), Resource("stone")], 1)
-pawn_shop = BlueCard("Baths", [], 3)
-cards = [timber_yard, pawn_shop]
-aquaduct = BlueCard("Aquaduct", [], 5, is_free_with=["Baths"])
+timber_yard = RawMaterial("Timber Yard", [Resource("wood"), Resource("stone")], 1)
+pawn_shop = CivilianStructure("Baths", [], 3)
+aquaduct = CivilianStructure("Aquaduct", [], 5, is_free_with=["Baths"])
+laboratory = ScientificStructure("Laboratory", [Resource("clay"), Resource("clay"), Resource("paper")], Science("gear"), is_free_with=["Workshop"])
 
 
-print 'is it free? %s' % (aquaduct.is_free_with_cards(cards), )
+cards = [timber_yard, pawn_shop, aquaduct]
+
+
+print 'is it free? %s' % (laboratory.is_free_with_cards(cards), )
 
 for card in cards:
 	print card.reward()
